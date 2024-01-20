3f036d15d5e88e9bc45021e590694b81af0e9a34
Dito Alfarizi <alfariziditol@gmail.com>
2024-01-20 18:55:55 +0700

    1 1

--- backend/routes/index.route.mjs
+++ backend/routes/index.route.mjs
@@ -4,14 +4,11 @@ import captchaRouter from "./captcha.route.mjs";
 
 const indexRouter = Router();
 
-
 indexRouter.get('/', (req, res) => {
     res.send('Server Deployed 🥳');
 })
 indexRouter.use('/users', usersRouter);
 
 indexRouter.use('/verify-recaptcha', captchaRouter);
-
-
 
 export default indexRouter;
