31d5de6582cea1ade4661321de2fbe0ceb800cd4
AlgonzaDJun <algonsadewangga@gmail.com>
2024-02-16 14:44:59 +0700

    baca blog post dari backend

--- src/routes/blog/Blog.js
+++ src/routes/blog/Blog.js
@@ -1,19 +1,38 @@
 import React from "react";
 import { useEffect, useState } from "react";
 import { motion } from "framer-motion";
-import ReactPaginate from 'react-paginate';
+import ReactPaginate from "react-paginate";
 //data
-import allBlogPosts from '../../data/allBlogPosts'
+import allBlogPosts from "../../data/allBlogPosts";
 //components
 import ScrollBtn from "../../helpers/ScrollBtn";
 import ResetLocation from "../../helpers/ResetLocation";
 import BlogPosts from "./BlogPosts";
+import { useQuery } from "@tanstack/react-query";
+
+const fetchBlog = async () => {
+  const response = await fetch(`${process.env.REACT_APP_API_URL}/posts`);
+  const data = await response.json();
+  return data;
+};
 
 const Blog = () => {
+  const { isLoading, isError, data } = useQuery({
+    queryKey: ["blog"],
+    queryFn: fetchBlog,
+    refetchIntervalInBackground: 1000,
+  });
+
+  const [allBlogPosts, setAllBlogPosts] = useState([]);
+
   const [itemOffset, setItemOffset] = useState(0);
   const [endOffset, setEndOffset] = useState(itemOffset + 6);
-  const [currentBlogPosts, setcurrentBlogPosts] = useState([...allBlogPosts].reverse().slice(itemOffset, endOffset));
-  const [pageCountPosts, setpageCountPosts] = useState(Math.ceil(allBlogPosts.length / 6));
+  const [currentBlogPosts, setcurrentBlogPosts] = useState(
+    [...allBlogPosts].reverse().slice(itemOffset, endOffset)
+  );
+  const [pageCountPosts, setpageCountPosts] = useState(
+    Math.ceil(allBlogPosts.length / 6)
+  );
 
   const handlePageClick = (event) => {
     const newOffset = (event.selected * 6) % allBlogPosts.length;
@@ -22,11 +41,30 @@ const Blog = () => {
   };
 
   useEffect(() => {
-    document.title = "Blog Mitrakom";
-    setEndOffset(itemOffset + 6);
-    setcurrentBlogPosts([...allBlogPosts].reverse().slice(itemOffset, endOffset));
-    setpageCountPosts(Math.ceil(allBlogPosts.length / 6));
+    if (data) {
+      // console.log(data)
+      const updatedAllProducts = data.data.map((product) => ({
+        ...product,
+        attributes: [], // Jika 'attributes' null, ganti dengan array kosong
+      }));
+      // console.log(updatedAllProducts)
+      setEndOffset(itemOffset + 6);
 
+      setAllBlogPosts(updatedAllProducts);
+      setcurrentBlogPosts(
+        [...updatedAllProducts].reverse().slice(itemOffset, endOffset)
+      );
+      setpageCountPosts(Math.ceil(updatedAllProducts.length / 6));
+    }
+  }, [data, endOffset, itemOffset]);
+
+  useEffect(() => {
+    document.title = "Blog Mitrakom";
+    // setEndOffset(itemOffset + 6);
+    // setcurrentBlogPosts(
+    //   [...allBlogPosts].reverse().slice(itemOffset, endOffset)
+    // );
+    // setpageCountPosts(Math.ceil(allBlogPosts.length / 6));
   }, [setEndOffset, endOffset, itemOffset]);
   return (
     <motion.main
@@ -37,9 +75,7 @@ const Blog = () => {
       transition={{ duration: 1 }}
     >
       <h2>Blog</h2>
-      <p className="blog-intro">
-        Informasi kemajuan teknologi paling update!
-      </p>
+      <p className="blog-intro">Informasi kemajuan teknologi paling update!</p>
       <section className="blog-grid">
         {currentBlogPosts.map((blogPost, index) => {
           return <BlogPosts key={index} blogPost={blogPost} />;
@@ -58,5 +94,5 @@ const Blog = () => {
       <ScrollBtn />
     </motion.main>
   );
-}
+};
 export default Blog;
--- src/routes/blog/BlogPosts.js
+++ src/routes/blog/BlogPosts.js
@@ -1,11 +1,24 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import ResetLocation from '../../helpers/ResetLocation'
+import ResetLocation from "../../helpers/ResetLocation";
 
 const BlogPosts = ({ blogPost }) => {
+  const potongKalimat = (kalimat) => {
+    return kalimat.split(" ").slice(0, 20).join(" ") + "...";
+  };
+
+  const formattedDate = new Date(blogPost.createdAt).toLocaleDateString(
+    "en-GB",
+    {
+      day: "numeric",
+      month: "numeric",
+      year: "numeric",
+    }
+  );
+
   return (
     <section className="blog-grid-item">
-      <img
+      {/* <img
         src={blogPost.img}
         alt={blogPost.name}
       />
@@ -19,9 +32,29 @@ const BlogPosts = ({ blogPost }) => {
       >
         <h3>{blogPost.name}</h3>
       </Link>
-      <p className="intro">{blogPost.intro}</p>
+      <p className="intro">{blogPost.intro}</p> */}
+
+      {/* BARU */}
+
+      <img src={blogPost.image} alt={blogPost.judul} />
+      <section className="credentials">
+        <p>{formattedDate}</p>
+        <p>by {blogPost.penulis}</p>
+      </section>
+      <Link
+        onClick={ResetLocation}
+        to={`/blog/${blogPost.judul.toLowerCase().replace(/[^a-zA-Z0-9 ]/g, '').replaceAll(" ", "-")}`}
+      >
+        <h3>{blogPost.judul}</h3>
+      </Link>
+      <p className="intro">
+        <div
+          dangerouslySetInnerHTML={{ __html: potongKalimat(blogPost.body) }}
+        />
+        {}
+      </p>
     </section>
   );
-}
+};
 
 export default BlogPosts;
